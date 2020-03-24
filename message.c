b78b77cbf935208ed3f9adcfd9e2a46800b3af14
ALievre <lievreagathe@gmail.com>
2020-03-24 16:46:38 +0100

    Fichiers initiaux

---
+++ message.c
@@ -0,0 +1,14 @@
+/* librairie standard ... */
+#include <stdlib.h>
+/* pour les entrées/sorties */
+#include <stdio.h>
+
+#include "message.h"
+
+void construire_message(char *message, char motif, int lg) {
+
+}
+
+void afficher_message(char *message, int lg) {
+
+}
---
+++ message.h
@@ -0,0 +1,4 @@
+
+
+void construire_message(char *message, char motif, int lg);
+void afficher_message(char *message, int lg);
